51fb045f58d787408694667fcb2946d3b3136db9
Lukas Läderach <lukas.laederach@3volutions.ch>
2017-04-05 11:25:47 +0200

    add basic oszillation

---
+++ Schwingungen/Form1.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace Schwingungen
+{
+    sealed partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.renderingTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // renderingTimer
+            // 
+            this.renderingTimer.Enabled = true;
+            this.renderingTimer.Interval = 10;
+            this.renderingTimer.Tick += new System.EventHandler(this.renderingTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1690, 320);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer renderingTimer;
+    }
+}
+
---
+++ Schwingungen/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace Schwingungen
+{
+    public sealed partial class Form1 : Form
+    {
+        private readonly List<PointF> _points1 = new List<PointF>();
+        private readonly List<PointF> _points2 = new List<PointF>();
+        private double _timeElapsed;
+
+        public Form1()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            var auslenkung1 = 50 + 30 * Math.Cos(2 * Math.PI * 1.5 * _timeElapsed);
+            var auslenkung2 = 50 + 30 * Math.Sin(2 * Math.PI * 1.5 * _timeElapsed);
+            e.Graphics.FillEllipse(Brushes.Red, 30, (float)auslenkung1, 6, 6);
+            e.Graphics.FillEllipse(Brushes.Green, 30, (float)auslenkung2, 6, 6);
+            _points1.Add(new PointF((float)(50 + 75 * _timeElapsed), (float)auslenkung1));
+            _points2.Add(new PointF((float)(50 + 150 * _timeElapsed), (float)auslenkung2));
+            if (_points1.Count > 1)
+            {
+                e.Graphics.DrawLines(Pens.Red, _points1.ToArray());
+            }
+            if (_points2.Count > 1)
+            {
+                e.Graphics.DrawLines(Pens.Green, _points2.ToArray());
+            }
+        }
+
+        private void renderingTimer_Tick(object sender, EventArgs e)
+        {
+            Invalidate();
+            _timeElapsed += renderingTimer.Interval * 0.001;
+            Text = Math.Round(_timeElapsed, 2).ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
